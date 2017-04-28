e27b8dd874745a61784992e74d8c9291ef0feaa6
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-04-28 21:36:24 +0700

    add let and var

---
+++ app/let.js
@@ -0,0 +1,21 @@
+console.log("hi");
+//block scope
+
+//var
+/*var score = 100;
+if (score > 50) {
+    var message = "Win";
+    console.log(message);
+}
+console.log(score);
+console.log(message);
+*/
+//let
+
+var score = 100;
+if (score > 50) {
+    let message = "Win";
+    console.log(message);
+}
+console.log(score);
+console.log(message);
